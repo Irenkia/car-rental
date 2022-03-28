693313629cfd8469c5a088b87c65e4ff8b3670ba
Iryna Tiutiun <tiutiuniryna@gmail.com>
2022-03-28 23:45:42 +0200

    fixed links in Footer file

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,10 @@ import "./styles.css";
 const Footer = () => {
   return (
     <div className="footer">
-      {/* <h2>Iryna Tiutiun</h2> */}
       <p>
         &#160; © <span>Iryna Tiutiun</span>&#160; 28 March 2022 &#160;
         <a
-          href="https://github.com/Irenkia/React-Intro-3"
+          href="https://github.com/Irenkia/car-rental"
           target="_blank"
           rel="noreferrer"
         >
@@ -16,7 +15,7 @@ const Footer = () => {
         </a>{" "}
         Design by{" "}
         <a
-          href="https://ru.depositphotos.com/39223635/stock-illustration-ice-abstract-geometric-vector-background.html"
+          href="https://react-landing-page-template.herokuapp.com/#features"
           target="_blank"
           rel="noreferrer"
         >
@@ -24,23 +23,6 @@ const Footer = () => {
           Template
         </a>
       </p>
-
-      {/* </p>
-      <a
-        href="https://github.com/Irenkia/React-Intro-3"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Github
-      </a>
-      <p>Design by</p>
-      <a
-        href="https://ru.depositphotos.com/39223635/stock-illustration-ice-abstract-geometric-vector-background.html"
-        target="_blank"
-        rel="noreferrer"
-      >
-        template
-      </a> */}
     </div>
   );
 };
